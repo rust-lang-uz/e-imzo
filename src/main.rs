65ad9bd53c07bb1e1e2bb34b409d5e0eba83cefb
bahrom04 <magdiyevbahrom@gmail.com>
2025-09-02 16:45:41 +0500

    chore: add clone trait
    chore: add new relese version

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,23 @@
 use chrono::{DateTime, Utc};
-// use chrono::{DateTime, Utc};
 use e_imzo::list_all_certificates;
 
 fn main() {
-    // env_logger::init();
-    // match list_all_certificates() {
-    //     Ok(pfx) => {
-    //         let a: Vec<_> = pfx.iter().map(|c| (c, c.get_alias())).collect();
-    //         println!("this is resut list_all_certificates; {a:?}");
-    //         pfx.iter().map(|c| (c, c.get_alias())).for_each(|(c, a)| {
-    //             let validfrom: Vec<_> = a.get("validfrom").unwrap().split(" ").filter_map(|date| {
-    //                 let year_month_day: Vec<_> = date.split(".").collect();
-    //                 Some(year_month_day)
-
-    //             }).collect();
-    //             // let valid_date: Vec<_> = validfrom[0].split(".").collect();
-
-    //             println!("CERT: {c:?}");
-    //             println!("ALIAS: {a:?}");
-    //             println!("-----");
-    //             println!("DATE: {:?}", validfrom);
+    env_logger::init();
+    match list_all_certificates() {
+        Ok(pfx) => {
+            let a: Vec<_> = pfx.iter().map(|c| (c, c.get_alias())).collect();
+            println!("this is resut list_all_certificates; {a:?}");
+            pfx.iter().map(|c| (c, c.get_alias())).for_each(|(c, a)| {
+                let validfrom: Vec<_> = a.get("validfrom").unwrap().split(" ").collect();
+                let mut year_month_day: Vec<_> = validfrom[0].split(".").collect();
+                year_month_day.reverse();
                 
-    //         });
-    //     }
-    //     Err(e) => println!("{e}"),
-    // }
-    // let ws_url = Url::parse("wss://127.0.0.1:64443/service/cryptapi");
+                println!("CERT: {c:?}");
+                println!("ALIAS: {a:?}");
+                println!("-----");
+                println!("DATE: {:?}", year_month_day.join("."));
+            });
+        }
+        Err(e) => println!("{e}"),
+    }
 }
